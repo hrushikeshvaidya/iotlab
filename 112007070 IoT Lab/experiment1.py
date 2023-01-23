e6359e616f098e7a22d9266cb25f9baf835a3ed7
hrushikeshvaidya <68425560+hrushikeshvaidya@users.noreply.github.com>
2023-01-23 13:22:15 +0530

    Delete 112007070 IoT Lab directory

--- 112007070 IoT Lab/experiment1.py
+++
@@ -1,9 +0,0 @@
-a = int(input('A = '))
-b = int(input('B = '))
-
-print(f'A + B = {a} + {b} = {a + b}')
-print(f'A - B = {a} - {b} = {a - b}')
-print(f'A * B = {a} * {b} = {a * b}')
-print(f'A / B = {a} / {b} = {a / b}')
-print(f'A % B = {a} % {b} = {a % b}')
-print(f'A // B = {a} // {b} = {a // b}')
--- 112007070 IoT Lab/experiment2.py
+++
@@ -1,7 +0,0 @@
-for i in range(10):
-    print(i)
-
-i = 0
-while i < 10:
-    print(i)
-    i += 1
